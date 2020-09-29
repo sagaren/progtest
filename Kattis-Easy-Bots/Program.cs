77a3494459e731fb28f9cfb55798ee8c64b71d1b
Saga Renqvist <sagarenqvist@gmail.com>
2020-09-29 17:10:37 +0200

    Initial check-in of module Kattis-Easy-Bots

---
+++ Kattis-Easy-Bots/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kattis_Easy_Bots
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine().ToLower(); 
+            if (inputLine.Length < 80) 
+            {
+                Console.WriteLine(RepeatedWord(inputLine));
+            }
+            else
+            {
+                Console.WriteLine("The maximun length of the line is 80 characters. Try again!");
+            }
+        }
+
+        public static string RepeatedWord (string inputLine)
+        {
+            string[] lineWithWords = inputLine.Split(' ');
+            string yes = "yes";
+            string no = "no";
+            
+            Dictionary<string, int> uniqueWords = new Dictionary<string, int>();
+
+            foreach (var word in lineWithWords)
+            {
+                if (!uniqueWords.ContainsKey(word))
+                {
+                    uniqueWords.Add(word, 1);
+                }
+
+                else
+                {
+                    uniqueWords[word] += 1;
+                }
+            }
+
+            if (uniqueWords.Where(i => i.Value != 1).Any())
+            {
+                return no;
+            }
+
+            else
+            {
+                return yes;
+            }
+          
+        }
+    }
+}
